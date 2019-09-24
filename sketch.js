29b7d01b082f144597df27810b00526f90109eb1
SagaraK <43074699+SagaraK@users.noreply.github.com>
2019-09-24 16:37:29 -0700

    classRoster

--- sketch.js
+++ sketch.js
@@ -1,23 +1,30 @@
-let x; 
+
+let classRoster = [{firstName:"Zainib", lastName:"Ahmed", color:"Blue", animal:"Giraffe", book:"The Book Thief", movie:"Amelie", game:"none", superHero:"The Hulk", randomFact:"I love to eat olives."},
+{firstName: "Toni",  lastName:"Anguiano",color:"blue", animal: "cat", book: "The Bell Jar", movie: "Horror",superHero: "batman", randomFact: "I like to hangout in cemeteries and research dark California history"},
+{firstName: "Camila",lastName: "Avina Garcia",color: "Green",animal:"Dog",book:"Match",movie:"Lady and the Tramp",game:" Guitar Hero",superHero:"Spiderman",randomFact:"I have a twin"},
+{firstName:"Eddson",lastName:"Jose",color:"midnight blue",animal:"pig",book:"The Dark Forest",movie:"The Fountain",game:"Halo Reach",superHero:"Superman",randomFact:"I am made up of stardust"},
+{firstName: "Lydia",lastName: "Lim",color: "blue",animal : "dog",book: "No",movie: "me before you",game: "No",superHero: "Thor",randomFact: " birds are tetrapods even though they only walk on two legs"},
+{firstName:"Chi", lastName:"Nguyen", color:"purple", animal:"bird", book:"Math Curse", movie:"Big Hero 6", game:"Checkers", superHeroe:"Big Hero 6", randomFact:"Lately this year, monsters have especially been occupying my mind, among other things."},
+{firstName: "Leon", lastName: "Quach", color: "silver", animal:"penguins", book: "Letters to a Young Poet", movie:"Kung fu Hustle", game: "Pokemon Crystal Version", superHero: "Batman", randomFact:"I have two pet guinea pigs"},
+{firstName: "Sierra",lastName: "Roe",color: "purple",animal: "dog",book: "phantom tollbooth",movie: "Naussica of the Valley of the Wind",game: "pokemon heartgold",superHero: "captain marvel",randomFact: "can run 7 minute mile"},
+{firstName: "Hao",lastName: "Rong",color: "hex(#4285F4)",animal: "otter",book: "A Brief History of Time",movie: "Angry Birds 2",game: "Final Fantasy XIV",superHero: "Tony Stark",randomFact: "I like cats."},
+{firstname:"Kaicong",lastname: "Tang",color: "black",animal: "dog",movie: "iron man123",game: "csgo",superHero : "iron man",randomFact: "love gaming"},
+{firstName: "Sumedh",lastName: "Vedanthi",color:"Blue" ,animal: "Crow",book: "Of Mice and Men",movie: "American Psycho",game: "Fruit Ninja" ,superHero: "Batman" ,randomFact: "im a black belt"},
+{firstName: "Chris",lastName: "Weber",color: "green",animal: "cat",book: "Hitchhiker's Guide to the Galaxy",movie: "Spiderman Into The Spiderverse",game: "The Legend of Zelda Breath of the Wild",superHero: "Iron Man",randomFact: "I'm vegetarian"},
+{firstName: "Andy", lastName: "Wu", color: "Red", animal:"Kiwi", book: "Romance of the Three Kingdoms", movie:"Shrek 2", game: "Super Mario 64", superHero: "Static Shock", randomFact:"I've had jaw correction surgery"},
+{firstName: "xiaowei",lastName: "zheng",color:" green",animal: "babies",book: "the little girl at the window",movie: "eat drink man woman",game: "none",superHero: "none",randomFact: "don't drink coke"},
+{firstName: "Hector", lastName: "Flores", Color: "Blue",Animal:"Dog",Book:"Captain Underpants", Movie:"Ace Ventura pet detective", Game:"Smash Brothers", superHero:"Flash", randomFact:"I am a Libra"},
+{firstName: "Matteo", lastName: "Leva", Color: "Black", Animal: "Cat", Book: "Pillars of the earth", Movie:"The Godfather", Game: "Witcher 3", superHero: "spider man", randomFact:"I'm disappointed in GOT season 8"},
+{firstName:"Kevin", lastName:"Sagara", color:"yellow", animal:"cat", book:"sherlock holmes", movie:"Shaun of the Dead", game:"osu!", superHero:"batman", randomFact:"learning japanese"},
+{firstName: "Anthony", lastName: "Valdovinos", color:"purple",animal: "eagle",book: "Hatchet",movie: "Titanic",game: "Starfox Adventures",superHero: "none", randomFact: "I studied Japanese for 4 years"},
+{firstName: "Rachel", lastName: "Centinaje", color: "green", animal:"dogs", book: "none", movie:"Drive", game: "Dishonored", superHero: "Deadpool", randomFact:"I went camping this weekend"},
+{firstName:"Alicia",lastName:"Breinke", color: "purple", animal: "dog", book:"The Art of Racing in the Rain", movie: "Spirited Away", superHero: "Wonder Woman", randomFact: "I want to live in another country someday"},
+{firstName: "asra", lastName: "siddiqui", color: "idk", animal: "chameleons", book: "codex 1962", movie: "pulp fiction", game: "idk", superHero: "hate em all", randomFact: "im grumpy"}];
 
 function  setup() {
-createCanvas(400,400); 
-background(0);
-x = 200;
+createCanvas(400,400);
+console.log("classroster 5" + classRoster[5]);
 }
 
 function draw() {
-background(0);
-drawEllipse(x);
-if(x < 400) {
-x++; 
-}
-else{
-x = 0; 
-}
-}
-
-function drawEllipse(_posX) {
-this.posX = _posX;
-ellipse(this.posX, 200, 100);
 }
